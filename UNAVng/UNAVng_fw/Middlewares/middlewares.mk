d6d47cb35434715412586526022fe0f9bbd18d76
Alessio Morale <alessiomorale@gmail.com>
2019-05-11 17:07:58 +0100

    Add littlefs to manage settings and data storage

--- UNAVng/UNAVng_fw/Middlewares/middlewares.mk
+++ UNAVng/UNAVng_fw/Middlewares/middlewares.mk
@@ -1,11 +1,23 @@
 USBDEVLIBRARYPATH = $(MIDDLEWARESPATH)/ST/STM32_USB_Device_Library
-
+LITTLEFSLIBRARYPATH = $(MIDDLEWARESPATH)/littlefs
 usbdevlibsrc = $(USBDEVLIBRARYPATH)/Core/Src/usbd_core.c \
 $(USBDEVLIBRARYPATH)/Core/Src/usbd_ctlreq.c \
 $(USBDEVLIBRARYPATH)/Core/Src/usbd_ioreq.c \
 $(USBDEVLIBRARYPATH)/Class/CDC/Src/usbd_cdc.c 
 
-C_SOURCES += $(usbdevlibsrc)
+
+
+littlefssrc = \
+$(LITTLEFSLIBRARYPATH)/lfs.c \
+$(LITTLEFSLIBRARYPATH)/lfs_util.c
+
+C_SOURCES += \
+$(usbdevlibsrc) \
+$(littlefssrc) 
 
 C_INCLUDES += -I$(USBDEVLIBRARYPATH)/Core/Inc \
--I$(USBDEVLIBRARYPATH)/Class/CDC/Inc
+-I$(USBDEVLIBRARYPATH)/Class/CDC/Inc \
+-I$(LITTLEFSLIBRARYPATH)
+
+
+
