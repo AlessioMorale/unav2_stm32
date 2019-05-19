698e26c24fa30c6058a5b204653db5d56f5d60af
Alessio Morale <alessiomorale@gmail.com>
2019-05-19 18:32:09 +0100

    #1 - wip, use new messages

--- UNAVng/UNAVng_fw/Core/Inc/modules/rosmotormodule.h
+++ UNAVng/UNAVng_fw/Core/Inc/modules/rosmotormodule.h
@@ -6,17 +6,22 @@
 #include <std_msgs/Float32.h>
 #include <std_msgs/Int16.h>
 #include <std_msgs/Int32.h>
-
+#include <unav2_msgs/BridgeConfig.h>
 #ifndef ROSMOTORMODULE_H
 #define ROSMOTORMODULE_H
 
 namespace unav::modules {
+#define ACK_QUEUE_LENGTH 10
+#define ACK_QUEUE_ITEM_SIZE (sizeof(unav2_msgs::BridgeConfig::_transactionId_type))
 class RosMotorModule : public BaseRosModule {
 public:
   void initialize();
 
 protected:
   void moduleThreadStart();
+  StaticQueue_t _ackStaticQueue;
+  uint8_t _ackQueueStorageArea[ ACK_QUEUE_LENGTH * ACK_QUEUE_ITEM_SIZE ];
+  QueueHandle_t _ackQueue;
   unav::controls::PID pidControllers[MOTORS_COUNT];
 };
 } // namespace unav::modules
--- UNAVng/UNAVng_fw/Core/Src/modules/rosmotormodule.cpp
+++ UNAVng/UNAVng_fw/Core/Src/modules/rosmotormodule.cpp
@@ -1,29 +1,46 @@
 #include "FreeRTOS.h"
 #include "main.h"
 #include "tim.h"
-#include <control_msgs/PidState.h>
 #include <mathutils.h>
 #include <modules/rosmotormodule.h>
 #include <ros.h>
 #include <rosserial_msgs/Log.h>
-#include <sensor_msgs/JointState.h>
-#include <std_msgs/Float32.h>
-#include <std_msgs/Int16.h>
-#include <std_msgs/Int32.h>
+#include <unav2_msgs/JointState.h>
+#include <unav2_msgs/JointCommand.h>
+#include <unav2_msgs/PIDConfig.h>
+#include <unav2_msgs/PIDState.h>
+#include <unav2_msgs/BridgeConfig.h>
+#include <unav2_msgs/MechanicalConfig.h>
+#include <unav2_msgs/OperationConfig.h>
+#include <std_msgs/UInt32.h>
 #include <stm32f4xx.h>
 #include <utils/timer.h>
 #include <drivers/encoder.h>
 
 namespace unav::modules {
+volatile bool commandUpdated = false;
+volatile bool pidUpdated = false;
+QueueHandle_t ackQueue = nullptr;
 
 void RosMotorModule::initialize() {
-  BaseRosModule::initialize(osPriority::osPriorityAboveNormal, 512);
+  BaseRosModule::initialize(osPriority::osPriorityAboveNormal, 1024);
+  _ackQueue = xQueueCreateStatic( ACK_QUEUE_LENGTH, ACK_QUEUE_ITEM_SIZE, _ackQueueStorageArea, &_ackStaticQueue);
+  ackQueue = _ackQueue;
 }
 
-void dummy(const std_msgs::Float32 &cmd_msg) {}
+
+void commandUpdatedCB(const unav2_msgs::JointCommand &cmd_msg) { 
+  commandUpdated = true;
+}
+
+void pidUpdatedCB(const unav2_msgs::PIDConfig &cmd_msg) { 
+  pidUpdated = true;
+  xQueueSend(ackQueue, (void*)&cmd_msg.transactionId, portMAX_DELAY);
+}
+
 
 void RosMotorModule::moduleThreadStart() {
-  uint32_t wait = 5;
+  uint32_t wait = 10;
   unav::utils::Timer timer;
   unav::drivers::Encoder encoders[MOTORS_COUNT] = {
     unav::drivers::Encoder(&TIM_ENC1),
@@ -37,69 +54,121 @@ void RosMotorModule::moduleThreadStart() {
   
   const uint32_t motor_channels[MOTORS_COUNT] = TIM_MOT_ARRAY_OF_CHANNELS;
 
-  sensor_msgs::JointState jstate;
+  unav2_msgs::JointState jstate;
 
  // creating arrays for the message
-  char *name[] = {"m1", "m2"};
-  double vel[] = {0, 0};
-  double pos[] = {0, 0};
-  double eff[] = {0, 0};
+  char *name[MOTORS_COUNT] = {"m1", "m2"};
+  float vel[MOTORS_COUNT] = {0};
+  float pos[MOTORS_COUNT] = {0};
+  float eff[MOTORS_COUNT] = {0};
+
+  float pid_output[MOTORS_COUNT] = {0};
+  float pid_error[MOTORS_COUNT] = {0};
+  float pid_p_term[MOTORS_COUNT] = {0};
+  float pid_i_term[MOTORS_COUNT] = {0};
+  float pid_d_term[MOTORS_COUNT] = {0};
+  float pid_i_min[MOTORS_COUNT] = {0};
+  float pid_i_max[MOTORS_COUNT] = {0};
+  float pid_timestep[MOTORS_COUNT] = {0};
+
+  float cmd[] = {0};
 
   const float ppr = 12.0f; 
   const float gearReduction = 51.5f;
   const float reduction = (ppr * gearReduction * 2.0f);
+  const float motLPF = 0.0f;
+  const float encLPF = 0.0f;
+  const int8_t pid_publish_rate = 10;
+  int8_t pid_rate_counter = 0;
+  bool publish_pidstatus = false;
+  std_msgs::UInt32 msg_ack;
+  unav2_msgs::PIDState pidstate;
 
-  ros::Subscriber<std_msgs::Float32> subMot("unav/mot1", dummy);
-  ros::Subscriber<std_msgs::Float32> subKp("unav/kp", dummy);
-  ros::Subscriber<std_msgs::Float32> subKd("unav/kd", dummy);
-  ros::Subscriber<std_msgs::Float32> subKi("unav/ki", dummy);
-  ros::Subscriber<std_msgs::Float32> subKiLimit("unav/kiLimit", dummy);
-  ros::Subscriber<std_msgs::Float32> subEncLPF("unav/enclpf", dummy);
-  ros::Subscriber<std_msgs::Float32> subMotLPF("unav/motlpf", dummy);
-
-  getNodeHandle().subscribe(subMot);
-  getNodeHandle().subscribe(subKp);
-  getNodeHandle().subscribe(subKd);
-  getNodeHandle().subscribe(subKi);
-  getNodeHandle().subscribe(subKiLimit);
-  getNodeHandle().subscribe(subEncLPF);
-  getNodeHandle().subscribe(subMotLPF);
-
+  ros::Subscriber<unav2_msgs::JointCommand> subCommand("unav2/control/joint_cmd", commandUpdatedCB);
+  ros::Subscriber<unav2_msgs::PIDConfig> subPID("unav2/config/pid", pidUpdatedCB);
   
-  subMot.msg.data = 0.0;
+  ros::Publisher pubJoints("unav2/status/joint", &jstate);
+  ros::Publisher pubPIDState("unav2/status/vel_pid", &pidstate);
+  ros::Publisher puback("unav2/status/ack", &msg_ack);
+
+  // setup all array based topics
 
   // assigning the arrays to the message
-  jstate.name = name;
   jstate.position = pos;
   jstate.velocity = vel;
   jstate.effort = eff;
 
   // setting the length
-  jstate.name_length = 2;
-  jstate.position_length = 2;
-  jstate.velocity_length = 2;
-  jstate.effort_length = 2;
-  control_msgs::PidState pidstate;
-  ros::Publisher pubJoints("unav/joint", &jstate);
-  ros::Publisher pubpid1("unav/pid", &pidstate);
+  jstate.position_length = MOTORS_COUNT;
+  jstate.velocity_length = MOTORS_COUNT;
+  jstate.effort_length = MOTORS_COUNT;
+  
+  // assigning arrays to the message
+  pidstate.output = pid_output;
+  pidstate.error = pid_error;
+  pidstate.timestep = pid_timestep;
+  pidstate.p_term = pid_p_term;
+  pidstate.i_term = pid_i_term;
+  pidstate.d_term = pid_d_term;
+  pidstate.i_min = pid_i_min;
+  pidstate.i_max = pid_i_max;
+
+  // setting the length
+  pidstate.output_length = MOTORS_COUNT;
+  pidstate.p_term_length = MOTORS_COUNT;
+  pidstate.i_term_length = MOTORS_COUNT;
+  pidstate.d_term_length = MOTORS_COUNT;
+  pidstate.i_max_length = MOTORS_COUNT;
+  pidstate.i_min_length = MOTORS_COUNT;
+  pidstate.error_length = MOTORS_COUNT;
+  pidstate.output_length = MOTORS_COUNT;
+  pidstate.timestep_length = MOTORS_COUNT;
+
+  // advertise and subscribe all topics
+  getNodeHandle().subscribe(subCommand);
+  getNodeHandle().subscribe(subPID);
+
   getNodeHandle().advertise(pubJoints);
-  getNodeHandle().advertise(pubpid1);
+  getNodeHandle().advertise(pubPIDState);
+  getNodeHandle().advertise(puback);
+
+  const float alphaMotor = motLPF > 0 ?  LPF_ALPHA(nominalDt, motLPF) : 1.0f;
+
   for(int32_t i = 0; i < MOTORS_COUNT; i++){
     encoders[i].setup();
     encoders[i].setReduction(reduction);
     pidControllers[i].setGains(1, 0, 0, 1);
     pidControllers[i].setRange(-1.0f, 1.0f);
+    encoders[i].applyFilter(nominalDt, encLPF);
   }
+
+  //while(!(pidUpdated)){
+  //  vTaskDelay(100);
+  //}
   while (true) {
     dt = timer.interval();
-    const float alphaMotor = subMotLPF.msg.data > 0 ?  LPF_ALPHA(nominalDt, subMotLPF.msg.data) : 1.0f;
-    float requiredSpeed = subMot.msg.data;
+    if(commandUpdated){
+      commandUpdated = false;
+      for(int i = 0; (i < MOTORS_COUNT) && (i < subCommand.msg.command_length); i++){
+        cmd[i] = subCommand.msg.command[i];
+      }
+    }
+    if(pidUpdated){
+      pidUpdated = false;
+      for(int i = 0; (i < MOTORS_COUNT); i++){
+        pidControllers[i].setGains(subPID.msg.vel_kp, subPID.msg.vel_ki, subPID.msg.vel_kd, subPID.msg.vel_kaw);
+      }
+    }
+    pid_rate_counter --;
+    if(pid_rate_counter <= 0){
+      pid_rate_counter = pid_publish_rate;
+      publish_pidstatus = true;
+    }
+
     for(int32_t i = 0; i < MOTORS_COUNT;i++){
-      encoders[i].applyFilter(nominalDt, subEncLPF.msg.data);
-      pidControllers[i].setGains(subKp.msg.data, subKi.msg.data, subKd.msg.data, subKiLimit.msg.data);
       float measuredSpeed = encoders[i].getVelocity();
       vel[i] = measuredSpeed;
-      float motorOutput = pidControllers[i].apply(requiredSpeed, measuredSpeed, dt, &pidstate);
+      float motorOutput = pidControllers[i].apply(cmd[i], measuredSpeed, dt);
       filteredOutput[i] = alphaMotor * (motorOutput - filteredOutput[i]) + filteredOutput[i];
       eff[i] = filteredOutput[i];
       uint32_t motoroutput = (uint32_t)(512.0 + filteredOutput[i] * 512.0);
@@ -108,10 +177,30 @@ void RosMotorModule::moduleThreadStart() {
         motoroutput = 0.0f;
       }
       __HAL_TIM_SET_COMPARE(&TIM_MOT, motor_channels[i], motoroutput);
+      // handle diagnostics and status reporting
       pos[i] = encoders[i].getPosition();
+      if(publish_pidstatus){
+        unav::controls::pid_status_t s;
+        pidControllers[i].getStatus(&s);
+        pid_output[i] = s.output;
+        pid_error[i] = s.error;
+        pid_p_term[i] = s.p_term;
+        pid_i_term[i] = s.i_term;
+        pid_i_max[i] = s.i_max;
+        pid_i_min[i] = s.i_min;
+        pid_d_term[i] = s.d_term;
+        pid_timestep[i] = s.timestep;
+      }
     }
-    pubpid1.publish(&pidstate);
+      
+      if(publish_pidstatus){
+        pubPIDState.publish(&pidstate);
+        publish_pidstatus = false;
+      }
     pubJoints.publish(&jstate);
+    while (xQueueReceive(_ackQueue, &msg_ack.data, 0)){
+      puback.publish(&msg_ack);
+    }
     vTaskDelayUntil(&c, wait);
   }
 }
