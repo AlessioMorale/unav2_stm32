2a560f1636aca364b3d952e7b7fcb36312058e08
Alessio Morale <alessiomorale@gmail.com>
2019-11-27 23:56:02 +0000

    start to sanitize folder names

--- firmware/middlewares/middlewares.mk
+++ firmware/middlewares/middlewares.mk
@@ -1,5 +1,4 @@
 USBDEVLIBRARYPATH = $(MIDDLEWARESPATH)/ST/STM32_USB_Device_Library
-LITTLEFSLIBRARYPATH = $(MIDDLEWARESPATH)/littlefs
 usbdevlibsrc = $(USBDEVLIBRARYPATH)/Core/Src/usbd_core.c \
 $(USBDEVLIBRARYPATH)/Core/Src/usbd_ctlreq.c \
 $(USBDEVLIBRARYPATH)/Core/Src/usbd_ioreq.c \
@@ -7,17 +6,11 @@ $(USBDEVLIBRARYPATH)/Class/CDC/Src/usbd_cdc.c
 
 
 
-littlefssrc = \
-$(LITTLEFSLIBRARYPATH)/lfs.c \
-$(LITTLEFSLIBRARYPATH)/lfs_util.c
-
 C_SOURCES += \
-$(usbdevlibsrc) \
-$(littlefssrc) 
+$(usbdevlibsrc)
 
 C_INCLUDES += -I$(USBDEVLIBRARYPATH)/Core/Inc \
 -I$(USBDEVLIBRARYPATH)/Class/CDC/Inc \
--I$(LITTLEFSLIBRARYPATH)
 
 
 
